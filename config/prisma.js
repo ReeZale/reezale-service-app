bf3294619cf457847e9c0f8ca8ee1e1c2bae2c28
zacrodney <zachary.rodney@pebbel.io>
2025-04-29 11:46:40 +0200

    updating prisma initialization and start commands

--- config/prisma.js
+++ config/prisma.js
@@ -1,5 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (!global.prisma) {
+  prisma = new PrismaClient();
+  if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma;
+  }
+} else {
+  prisma = global.prisma;
+}
 
 module.exports = prisma;
