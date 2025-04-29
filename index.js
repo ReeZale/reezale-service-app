37a3e8611132da514d7fe518a86daed17245fd34
zacrodney <zachary.rodney@pebbel.io>
2025-04-29 14:50:30 +0200

    adding prisma command to start command

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const router = require("./router");
-const prisma = require("./config/prisma");
+//const router = require("./router");
+//const prisma = require("./config/prisma");
+
+console.log("Initializing app");
 
 const app = express();
 
+console.log("Initializing cors");
+
 app.use(cors({ origin: true, credentials: true }));
+
+console.log("Initializing json");
+
 app.use(express.json());
+
+console.log("Initializing routes");
 
 app.get("/health", (_, res) => res.send("✅ Healthy"));
 app.get("/", (_, res) => res.send("✅ App is running"));
 
-app.use("/api/v1", router);
+//app.use("/api/v1", router);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.WEBSITES_URL || 8080;
+
+console.log("Port from .env", PORT);
 console.log(`Starting server on port ${PORT}`);
 
 app.listen(PORT, () => {
